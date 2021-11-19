c6879c3357dea0383e6641dc23ab37814c77e2b4
Ibrahim Alvarez <ibrahim.rodrigo27@gmail.com>
2021-11-19 15:58:05 -0800

    added async functionality

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,12 +1,41 @@
 import React, {useState} from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+
+//const popover = (
+//    <Popover id="popover-basic">
+//      <Popover.Header as="h3">Popover right</Popover.Header>
+//      <Popover.Body>
+//        And here's some <strong>amazing</strong> content. It's very engaging.
+//        right?
+//      </Popover.Body>
+//    </Popover>
+//  );
+//  
+//  const Example = () => (
+//    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+//      <Button variant="success">Click me to see</Button>
+//    </OverlayTrigger>
+//  );
+
+
 export default function SummaryForm(){
     const [tcChecked, setTcChecked] = useState(false);
 
+    const popover = (
+        <Popover id="popover-basic">
+          <Popover.Body>No ice cream will be actually delivered</Popover.Body>
+        </Popover>
+      );
+
     const checkboxLabel = (
         <span>
-            I agree to <span style={{color: 'blue'}}> Terms and Conditions</span>
+            I agree to 
+            <OverlayTrigger placement='right' overlay={popover}>
+                <span style={{color: 'blue'}}> Terms and Conditions</span>
+            </OverlayTrigger>
         </span>
     );
 
@@ -25,4 +54,4 @@ export default function SummaryForm(){
                 </Button>
         </Form>
     );
-}
+};
