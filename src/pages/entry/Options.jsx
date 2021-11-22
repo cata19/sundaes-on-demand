4828d94d03f5d0531f34fd634923d9a7174f08fc
Ibrahim Alvarez <ibrahim.rodrigo27@gmail.com>
2021-11-22 15:37:56 -0800

    Code and tests for scoop options images from server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,19 +1,31 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
 
-export default function Options({optionType}){
-    const [items, setItems] = useState([]);
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
 
-    //optionType is 'scoops' or 'toppings'
-    useEffect(()=>{
-        axios
-        .get(`http://localhost:3030/${optionType}`)
-        .then((responce) => setItems(response.data))
-        .catch((error) => {
-            //TODO: handle erros response
-        });
-    }, [optionType]);
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
 
-    return <div />
+  // TODO: replace `null` with ToppingOption when available
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
 
-};
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
