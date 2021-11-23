d491065b4671b598f67361b1b6844f615a9f583b
Ibrahim Alvarez <ibrahim.rodrigo27@gmail.com>
2021-11-22 16:38:30 -0800

    Created tests and set up for toppings

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 
 import Options from '../Options';
 
-test('It displays an image for each scoop from the server', async() => {
+test('displays an image for each scoop from the server', async() => {
     render(<Options optionType='scoops'/>);
 
     //find images
@@ -12,4 +12,16 @@ test('It displays an image for each scoop from the server', async() => {
     //confirm alt text of images
     const altText = scoopImages.map((element)=> element.alt);
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays an image for each topping from the server', async() => {
+    render(<Options optionType='toppings'/>);
+
+    //find images
+    const toppingImages = await screen.findAllByRole('img', {name: /topping$/i});
+    expect(toppingImages).toHaveLength(3);
+
+    //confirm alt text of images
+    const altText = toppingImages.map((element) => element.alt);
+    expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
 });
